3948e787bc824f952350a2ee7376d7bb8cc0a834
Krzysztof Pawelec <krzysztof.pawelec@xtb.com>
2025-03-25 14:26:59 +0100

    main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 def factorials(n):
-    factorial =1
+    factorial = 1
     for i in range(1, n + 1):
         factorial *= i
         yield factorial
@@ -8,6 +8,7 @@ def factorials(n):
 def main():
     for i, factorial in enumerate(factorials(100)):
         print(f"{i + 1}! = {factorial}")
+
 
 if __name__ == "__main__":
     main()
